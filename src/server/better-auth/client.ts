75621077ce51b7f4d2cd9bbfc7d8fe6fb020d449
victorbratov <victor@bratov.com>
2026-03-21 13:59:46 +0000

    fix: build errors

--- src/server/better-auth/client.ts
+++ src/server/better-auth/client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseUrl: "http://localhost:3000",
+  baseURL: "http://localhost:3000",
 }
 );
 
